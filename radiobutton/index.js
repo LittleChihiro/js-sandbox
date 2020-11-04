2073cedac0cf496b484f909cac2df377534c7f16
chihiro0109 <jasmine.moradiafkan@gmail.com>
2020-11-04 21:00:04 +0100

    RADIOBUTTOM update!

--- radiobutton/index.js
+++ radiobutton/index.js
@@ -1,18 +1,17 @@
-function boo() {
-    console.log ('boo')
-    a
+function doIt() {
+    let radioButtons = document.getElementsByName('element')
 
-
-
-
-
-
-
-
-    if (condition) {
-        
+for (let index = 0; index < radioButtons.length; index++) {
+    const radio = radioButtons[index];
+    if (radio.checked) {
+        let value = radio.value
+        if (value == 54) {
+            alert('yeeeeey!')
+        } else {
+            alert('neeeiiiiinnnnn :(')
+        }
     }
-
-
-
+    
+    
+}
 }
